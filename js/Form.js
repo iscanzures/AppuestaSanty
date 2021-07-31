2b6ff65d1861061826118b7e3f03cefc4d47470f
iscanzures <iscanzures@gmail.com>
2021-07-31 10:05:47 -0500

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,40 @@
+class Form {
+  constructor() {
+    this. title = createElement('h2')
+    this. hula = createElement('h2')
+  
+    
+    this. input = createInput("Name");
+    this. button = createButton('Play');
+    
+  }
+hide(){
+  this.title.hide();
+  this.input.hide();
+  this.button.hide();
+}
+  display(){
+    
+    this.title.html("Juego de Carreras de Autos");
+    this.title.position(130, 0);
+  
+    
+   this.input.position(130, 160);
+    this.button.position(250, 200);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+
+     player.name = this.input.value();
+      
+      playerCount+=1;
+      player.index=playerCount
+      player.update()
+      player.updateCount(playerCount);
+      this.hula.html("hula " + player.name )
+     this.hula.position(130, 160)
+    });
+
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-70,displayHeight-80);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw()
+{
+}
